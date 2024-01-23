c03af776dd62b10bed649a34cd4ef3c05a44b3d4
LirazDev <liraz@lirazganon.com>
2024-01-23 02:29:55 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fast/",
+  base: "https://lirazdev.github.io/fast/",
   plugins: [react()],
 });
